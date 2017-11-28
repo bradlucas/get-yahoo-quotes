63a181d5a28cd21b219c6b69d2fb23b090d04c39
mertdeg <mertdeg@gmail.com>
2017-11-28 08:01:36 -0800

    Fixing small bug in the script
    Changing Tr to tr makes the script work on CentOS 7

--- get-yahoo-quotes.sh
+++ get-yahoo-quotes.sh
@@ -49,7 +49,7 @@ function getCrumb () {
   # grep for the CrumbStore line
   # then copy out the value
   # lastly, remove any quotes
-  echo -en "$(curl -s --cookie-jar $cookieJar $1)" | Tr "}" "\n" | grep CrumbStore | cut -d':' -f 3 | sed 's+"++g'
+  echo -en "$(curl -s --cookie-jar $cookieJar $1)" | tr "}" "\n" | grep CrumbStore | cut -d':' -f 3 | sed 's+"++g'
 }
 
 # TODO If crumb is blank then we probably don't have a valid symbol
